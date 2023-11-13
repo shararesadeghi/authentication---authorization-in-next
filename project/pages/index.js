f2511e65ecbae91703875d53985a6d0f6a9dc8f2
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-14 00:25:51 +0330

    dashboard route & page created

--- project/pages/index.js
+++ project/pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <button><Link href="/dashboard">Dashboard</Link></button>
      <button><Link href="/signup">Sign Up</Link></button>
      <button><Link href="/signin">Sign In</Link></button>
     </div>
