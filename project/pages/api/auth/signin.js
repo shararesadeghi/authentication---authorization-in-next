3c4e57454053e48c9d66e43479bff0f86032bd6c
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-14 00:06:44 +0330

    serialized token & set cookie in signin api completed

--- project/pages/api/auth/signin.js
+++ project/pages/api/auth/signin.js
@@ -1,6 +1,8 @@
 import { verifyPassword } from "../../../utils/auth";
 import connectDB from "../../../utils/connectDB";
 import User from "./../../../models/User";
+import { sign } from "jsonwebtoken";
+import { serialize } from "cookie";
 
 async function handler(req, res) {
   if (req.method !== "POST") return;
@@ -15,6 +17,8 @@ async function handler(req, res) {
   }
 
   const { email, password } = req.body;
+  const secretKey = process.env.SECRET_KEY;
+  const expiration = 24 * 60 * 60;
 
   if (!email || !password) {
     return res.status(422).json({ status: "failed", message: "Invalid Data" });
@@ -31,13 +35,27 @@ async function handler(req, res) {
   const isValid = await verifyPassword(password, user.password);
 
   if (!isValid) {
-    return res
-      .status(422)
-      .json({
-        status: "failed",
-        message: "Username or password is not corecct!",
-      });
+    return res.status(422).json({
+      status: "failed",
+      message: "Username or password is not corecct!",
+    });
   }
+
+  const token = sign({ email }, secretKey, { expiresIn: expiration });
+
+  const serialized = serialize("token", token, {
+    httpOnly: true,
+    maxAge: expiration,
+    path: "/",
+  });
+  res
+    .status(200)
+    .setHeader("Set-Cookie", serialized)
+    .json({
+      status: "success",
+      message: "Logged in!",
+      data: { email: user.email },
+    });
 }
 
 export default handler;
