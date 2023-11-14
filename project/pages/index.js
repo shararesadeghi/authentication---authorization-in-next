e183173bfd0ae8a29dc0cc2ec75fd834867f92ee
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-15 00:18:28 +0330

    show buttons according authorization

--- project/pages/index.js
+++ project/pages/index.js
@@ -1,24 +1,43 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") setIsLoggedIn(true);
+      });
+  }, []);
   const signOutHandler = async () => {
     const res = await fetch("/api/auth/signout");
     const data = await res.json();
-    console.log(data);
+    if (data.status === "success") setIsLoggedIn(false);
   };
   return (
     <div className={styles.container}>
-      <button>
-        <Link href="/dashboard">Dashboard</Link>
-      </button>
-      <button>
-        <Link href="/signup">Sign Up</Link>
-      </button>
-      <button>
-        <Link href="/signin">Sign In</Link>
-      </button>
-      <button onClick={signOutHandler}>Log Out</button>
+      {isLoggedIn ? (
+        <>
+          <button>
+            <Link href="/dashboard">Dashboard</Link>
+          </button>
+          <button onClick={signOutHandler}>Log Out</button>
+        </>
+      ) : null}
+
+      {!isLoggedIn ? (
+        <>
+          <button>
+            <Link href="/signup">Sign Up</Link>
+          </button>
+          <button>
+            <Link href="/signin">Sign In</Link>
+          </button>
+        </>
+      ) : null}
     </div>
   );
 }
