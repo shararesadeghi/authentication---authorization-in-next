1f75d70bb6840c73cbbe3fdad9b325109bf3dbf3
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-15 00:04:26 +0330

    create signout api & buttin signout

---
+++ project/pages/api/auth/signout.js
@@ -0,0 +1,13 @@
+import { serialize } from "cookie";
+async function handler(req, res) {
+  if (req.method !== "GET") return;
+
+  const serialized = serialize("token", "", { path: "/", maxAge: 0 });
+
+  res
+    .status(200)
+    .setHeader("Set-Cookie", serialized)
+    .json({ status: "success", message: "Logged Out!" });
+}
+
+export default handler;
--- project/pages/index.js
+++ project/pages/index.js
@@ -1,12 +1,24 @@
-import Link from 'next/link';
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const signOutHandler = async () => {
+    const res = await fetch("/api/auth/signout");
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <div className={styles.container}>
-      <button><Link href="/dashboard">Dashboard</Link></button>
-     <button><Link href="/signup">Sign Up</Link></button>
-     <button><Link href="/signin">Sign In</Link></button>
+      <button>
+        <Link href="/dashboard">Dashboard</Link>
+      </button>
+      <button>
+        <Link href="/signup">Sign Up</Link>
+      </button>
+      <button>
+        <Link href="/signin">Sign In</Link>
+      </button>
+      <button onClick={signOutHandler}>Log Out</button>
     </div>
-  )
+  );
 }
