c504ca19b54c1db3d4d25146e46d886ee52dba82
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-15 01:15:48 +0330

    ui for dashbord created

--- project/pages/dashboard.js
+++ project/pages/dashboard.js
@@ -1,8 +1,46 @@
-import React from "react";
+import { useState } from "react";
 import { verifyToken } from "../utils/auth";
 
-const Dashboard = () => {
-  return <div>Dashborad</div>;
+const Dashboard = ({ result }) => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async () => {
+    const res = await fetch("/api/update-info", {
+      method: "POST",
+      body: JSON.stringify({ name, lastName, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h3>Dashborad</h3>
+      <p>Your email is {result.email}</p>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Lastname"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={submitHandler}>Submit</button>
+    </div>
+  );
 };
 
 export default Dashboard;
