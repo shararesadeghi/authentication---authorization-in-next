9ff477de18a977acb79b1a839326c1b2f93614bf
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-14 17:04:54 +0330

    server side authorization in dashboard page

--- project/pages/dashboard.js
+++ project/pages/dashboard.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { verifyToken } from "../utils/auth";
 
 const Dashboard = () => {
-    return (
-        <div>
-            Dashborad
-        </div>
-    );
+  return <div>Dashborad</div>;
 };
 
 export default Dashboard;
+
+export async function getServerSideProps(context) {
+  const { token } = context.req.cookies;
+  const secretKey = process.env.SECRET_KEY;
+  const result = verifyToken(token, secretKey);
+
+  if (!result) {
+    return { redirect: { destination: "/signin", permanent: false } };
+  }
+  return {
+    props: { result },
+  };
+}
